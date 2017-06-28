ff09e71a7ff04cd397638036d480fd381f1c9957
Ricardo Bravo <rbravo@datiobd.com>
2017-06-28 10:33:05 +0200

    Update linearmodel.py

--- app/linearmodel.py
+++ app/linearmodel.py
@@ -1,49 +1,47 @@
-print(__doc__)
+from sklearn import linear_model
+import redis
+import sys
+# generate random data
 
+import random
 
-# Code source: Jaques Grobler
-# License: BSD 3 clause
+def main(redis_server, process, key):
+    # let's create a linear function with some error called f
+    def f(x):
+        res = x* 25 + 3
+        error = res * random.uniform(-0.01, 0.01) # you can play with the error to see how it affects the result
+        return res + error
 
+    values = []
+    # now using f we are going to create 300 values.
+    for i in range(0, 300):
+        x = random.uniform(1, 1000)
+        y = f(x)
+        values.append((x, y))
 
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import datasets, linear_model
+    regr = linear_model.LinearRegression()
+    # split the values into two series instead a list of tuples
+    x, y = zip(*values)
+    max_x = max(x)
+    min_x = min(x)
+    # split the values in train and data.
+    train_data_X = map(lambda x: [x], list(x[:-20]))
+    train_data_Y = list(y[:-20])
+    test_data_X = map(lambda x: [x], list(x[-20:]))
+    test_data_Y = list(y[-20:])
+    # feed the linear regression with the train data to obtain a model.
+    regr.fit(train_data_X, train_data_Y)
+    # check that the coeffients are the expected ones.
+    m = regr.coef_[0]
+    b = regr.intercept_
+    res=' y = {0} * x + {1}'.format(m, b)
+    connection = redis.StrictRedis(host=redis_server, port=6379, db=0)
+    connection.hset(process, key, res)
+    print(connection.hget(process,key))
 
-# Load the diabetes dataset
-diabetes = datasets.load_diabetes()
-
-
-# Use only one feature
-diabetes_X = diabetes.data[:, np.newaxis, 2]
-
-# Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
-
-# Split the targets into training/testing sets
-diabetes_y_train = diabetes.target[:-20]
-diabetes_y_test = diabetes.target[-20:]
-
-# Create linear regression object
-regr = linear_model.LinearRegression()
-
-# Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
-
-# The coefficients
-print('Coefficients: \n', regr.coef_)
-# The mean squared error
-print("Mean squared error: %.2f"
-      % np.mean((regr.predict(diabetes_X_test) - diabetes_y_test) ** 2))
-# Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
-
-# Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, regr.predict(diabetes_X_test), color='blue',
-         linewidth=3)
-
-plt.xticks(())
-plt.yticks(())
-
-plt.show()
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: {} <redis_server> <process> <key>".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
