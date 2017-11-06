fec67a8dbad9a6b7f60a62de9ebf7bb61d228589
Ricardo Bravo <rbravo@datiobd.com>
2017-11-06 10:43:24 +0100

    Update task.sh

--- app/task.sh
+++ app/task.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #python /app/task.py	$1
-python /app/linearmodel.py $1 $2 $3 
-#. /venv/bin/activate
-#/venv/bin/python /app/linearmodel.py $1 $2 $3 
+#python /app/linearmodel.py $1 $2 $3 
+. /venv/bin/activate
+/venv/bin/python /app/linearmodel.py $1 $2 $3 
